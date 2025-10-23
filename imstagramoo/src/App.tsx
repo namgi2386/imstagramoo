15c30f0e151feec83ad4366280024731879a5fcb
imnamgi <happynj2697@gmail.com>
2025-10-24 00:48:43 +0900

    Add: shadcn/ui initial setting #2

--- imstagramoo/src/App.tsx
+++ imstagramoo/src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
-      <div>init</div>
+      <div className="bg-rose-300 text-2xl">tailwindcss-init</div>
+      <Button>shadcn-button-init</Button>
+      <div className="text-primary bg-muted border-destructive border-2">
+        css-test
+      </div>
     </>
   );
 }
