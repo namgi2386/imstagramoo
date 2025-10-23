44ad5ee1ed3861381b4dcdc74d469049ebebe440
imnamgi <happynj2697@gmail.com>
2025-10-24 01:11:19 +0900

    Add: shadcn/ui Toast #2

--- imstagramoo/src/App.tsx
+++ imstagramoo/src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
 
 function App() {
   return (
     <>
+      <Toaster />
       <div className="bg-rose-300 text-2xl">tailwindcss-init</div>
-      <Button>shadcn-button-init</Button>
+      <Button
+        variant={"destructive"}
+        onClick={() => toast("toast-test", { position: "top-center" })}
+      >
+        shadcn-button-init
+      </Button>
       <div className="text-primary bg-muted border-destructive border-2">
         css-test
       </div>
