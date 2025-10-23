adeaae581e6b1bbd2a7627d1d1723454dec4273b
imnamgi <happynj2697@gmail.com>
2025-10-24 01:45:38 +0900

    Feat: router layout test #2

--- imstagramoo/src/App.tsx
+++ imstagramoo/src/App.tsx
@@ -1,15 +1,26 @@
-import { Route, Routes } from "react-router";
+import { Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import Indexpage from "@/pages/index-page";
 import SignInPage from "@/pages/sign-in-page";
 import SignUpPage from "@/pages/sign-up-page";
 
+function AuthLayout() {
+  return (
+    <div>
+      <header>인증</header>
+      <Outlet />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Indexpage />} />
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
     </Routes>
   );
 }
