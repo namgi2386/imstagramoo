bdf7bb8e0b12f31c4537e3467b9b13bbbdc6764a
imnamgi <happynj2697@gmail.com>
2025-10-25 01:43:30 +0900

    Feat: zustand-middelware-combine #03

--- imstagramoo/src/store/count.ts
+++ imstagramoo/src/store/count.ts
@@ -1,29 +1,23 @@
 // src/store/count.ts
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
-
-export const useCountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      set((store) => ({
-        count: store.count + 1,
-      }));
+export const useCountStore = create(
+  combine({ count: 0 }, (set, get) => ({
+    actions: {
+      increase: () => {
+        set((store) => ({
+          count: store.count + 1,
+        }));
+      },
+      decrease: () => {
+        set((store) => ({
+          count: store.count - 1,
+        }));
+      },
     },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+  })),
+);
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
