73bbefc193f7d3d34a30896a3022ed4e0c6e2311
imnamgi <happynj2697@gmail.com>
2025-10-25 21:16:16 +0900

    Feat: connect tanstackquery todosAPI #04

--- imstagramoo/src/pages/todo-list-page.tsx
+++ imstagramoo/src/pages/todo-list-page.tsx
@@ -1,14 +1,18 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
-import { useTodos } from "@/store/todos";
+import { useTodosData } from "@/hooks/queries/use-todos.data";
 
 export default function TodoListPage() {
-  const todos = useTodos();
+  const { data: todos, isLoading, error } = useTodosData();
+
+  if (error) return <div>오류발생</div>;
+  if (isLoading) return <div>로딩중</div>;
+
   return (
     <div className="flex flex-col gap-5 p-5">
       <h1 className="text-2xl">todo</h1>
       <TodoEditor />
-      {todos.map((t) => (
+      {todos?.map((t) => (
         <TodoItem key={t.id} {...t} />
       ))}
     </div>
