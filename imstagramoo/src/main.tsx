6ac2dc0658f1abb673a6ac6473941195b7e440cf
imnamgi <happynj2697@gmail.com>
2025-10-26 14:09:14 +0900

    Conf: caching default setting #04

--- imstagramoo/src/main.tsx
+++ imstagramoo/src/main.tsx
@@ -5,7 +5,18 @@ import { BrowserRouter } from "react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0, // 가장 최신상태를 항상유지하기 위해 0이 좋음
+      gcTime: 5 * 60 * 1000, // 5분정도가 적당히 여유있음
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
